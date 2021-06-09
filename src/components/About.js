dce89115bfd4ae7feb932284e105316585ff61ae
Cleisson <cleissonconstc@gmail.com>
2021-06-09 15:31:17 -0300

    :tada: first commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+      <h4>Versão 1.1</h4>
+      <Link to='/'>Voltar</Link>
+    </div>
+  )
+}
+
+export default About
